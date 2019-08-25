56536ff46b4b33f88c9d196699c75ace9a03c481
Maxwel5 <maxwellwafulasim@gmail.com>
2019-08-25 23:29:25 +0300

    Importing output and an event emitter on search form

--- src/app/github-search-form/github-search-form.component.ts
+++ src/app/github-search-form/github-search-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-github-search-form',
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-search-form.component.css']
 })
 export class GithubSearchFormComponent implements OnInit {
+  @Output() emitFind = new EventEmitter<any>()
 
+  findUser:string;
   constructor() { }
+
+  find()
 
   ngOnInit() {
   }
