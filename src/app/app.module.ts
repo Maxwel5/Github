e7e519785b9d3544462c48a1500f71b97db25130
Maxwel5 <maxwellwafulasim@gmail.com>
2019-08-25 22:59:21 +0300

    Generating search form folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { GithubNavbarComponent } from './github-navbar/github-navbar.component';
+import { GithubSearchFormComponent } from './github-search-form/github-search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    GithubNavbarComponent
+    GithubNavbarComponent,
+    GithubNavbarComponent,
+    GithubSearchFormComponent
   ],
   imports: [
     BrowserModule,
